9178e78a877edd5bc4569b2739e7cddf83f0690c
smsanagustin <smsanagustin@up.edu.ph>
2023-10-31 01:12:34 +0800

    created a reply function

--- chat.erl
+++ chat.erl
@@ -1,12 +1,25 @@
 -module(chat).
 -export([init_chat/0, receiver/1, init_chat2/1, sendMessage/3]).
 
+% initializes current node (host node)
 init_chat() ->
     UserInput = io:get_line("Enter your name: "),
     % remove \n
     UserName = string:strip(UserInput, both, $\n), 
     register(receiver, spawn(chat, receiver, [UserName])).
 
+% gets reply from the user through user input
+getReply(UserName, Sender_Pid) ->
+    io:format("~s", [UserName]),
+    ReplyInput = io:get_line(": "),
+    % removes \n
+    Reply = string:strip(ReplyInput, both, $\n),
+
+    % send reply to the other user
+    Sender_Pid ! {Reply, UserName},
+    receiver(UserName).
+
+% called when host is initialized to allow it to receive messages
 receiver(UserName) ->
     receive
         % when user receives bye, chat will be terminated
@@ -22,33 +35,19 @@ receiver(UserName) ->
 
         {"", UserName2, Sender_Pid} ->
             io:format("You are now connected to ~s.~n", [UserName2]),
-
-            % allow reply from the current user
-            io:format("~s", [UserName]),
-            ReplyInput = io:get_line(": "),
-            % removes \n
-            Reply = string:strip(ReplyInput, both, $\n),
-
-            % send reply to the other user
-            Sender_Pid ! {Reply, UserName},
-            receiver(UserName);
+            % get reply from the current user
+            getReply(UserName, Sender_Pid);
 
         {Message, UserName2, Sender_Pid} ->
             % receiver will print on the terminal the message it received
             io:format("~s: ~s~n", [UserName2, Message]),
 
             % get reply from the current user when it received a message:
-            io:format("~s", [UserName]),
-            ReplyInput = io:get_line(": "),
-            % removes \n
-            Reply = string:strip(ReplyInput, both, $\n),
-
-            % send reply and current user to the sender
-            Sender_Pid ! {Reply, UserName},
-            receiver(UserName)
+            getReply(UserName, Sender_Pid)
     end.
 
 % ReceiverNode is the node of the user you want to send a message to
+% init_chat2 connects current node to the host node
 init_chat2(ReceiverNode) ->
     % get user's name
     UserInput2 = io:get_line("Enter your name: "),
@@ -58,12 +57,6 @@ init_chat2(ReceiverNode) ->
     % send an empty string initially to connect two nodes
     spawn(chat, sendMessage, ["", UserName2, ReceiverNode]).
 
-% if sender sends a "bye" message, bye will be sent to receiver; chat will end
-% sendMessage("bye", UserName2, ReceiverNode) ->
-%     UserName2 = UserName2,
-%     {receiver, ReceiverNode} ! bye,
-%     io:format("ok");
-
 % used to send message to the other user
 sendMessage(Message, UserName2, ReceiverNode) ->
     % send Message to receiver
@@ -71,9 +64,12 @@ sendMessage(Message, UserName2, ReceiverNode) ->
 
     % check if there are replies from the receiver
     receive
+        % used to halt process when the other user receives "bye"
+        % see line 19 to understand what this code is for
         bye ->
             halt();
-        
+
+        % halts process when this process itself receives "bye"
         {"bye", UserName} ->
             UserName = UserName,
             io:format("Your partner has disconnected.~n"),
